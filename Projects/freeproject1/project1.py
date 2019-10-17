b310b1b64ec97fa95f5318020052d42fc6db6a38
Rachel <radonwrites@gmail.com>
2019-10-16 23:28:48 -0400

    Free project added!

---
+++ Projects/freeproject1/project1.py
@@ -0,0 +1,22 @@
+
+# original starter program
+print('Hello World')
+#%%
+#modified
+print("Greetings, stranger. State your name.")
+def mynameis(x):
+    print("Affirmative, " + x + ". I read you.")
+
+#%%
+#attempted simplification of input for the user (trying to reference the quiz questions,
+# how when you run the program you can just enter input and it reads it)
+#def converse():
+ #   try:
+  #      answer = input("State your name \n")
+   #     answer = str(answer)
+    #except TypeError:
+     #   if(type(answer) != str):
+        #    print("I... don't understand. Please repeat")
+         #   return converse()
+    #return answer
+#%%
