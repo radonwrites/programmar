2e414f20f0a874066b81a2565fce7fad440a32cb
Rachel <radonwrites@gmail.com>
2019-10-23 20:37:21 -0400

    in class

--- Class/class_and_json.py
+++ Class/class_and_json.py
@@ -1,6 +1,6 @@
 #%%
 class MyClass:
-    """A simple example class"""
+    """A simple example class""" #shows up on hover later
     i = 12345
     def __init__(self, name):
         self.name = name
@@ -31,5 +31,22 @@ with open('out.json', 'r') as nfh:
     obj = json.load(nfh)
     print(obj)
     print(obj["president"]["name"])
+
+#%%
+from PIL import Image
+#%%
+#'alpha' is basically masking
+mode = 'RGBA'
+size = (100,100)
+color = (10,50,50,255)
+ourimage = Image.new(mode, size, color)
+ourimage
+#%%
+ourimage.putpixel((50,50),(255,255,255,100))
+ourimage
+#%%
+# a byte is 8-bits (255 is number of bytes in 8bit or something)
+# check if PIL can support 16-bit?
+ourimage.save('allblack.png')
 
 #%%
