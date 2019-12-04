1b579630d21003cf594415def4f663321794fba4
Rachel <radonwrites@gmail.com>
2019-12-04 09:28:47 -0500

    oops forgot to update - trying to make random work

---
+++ FinalProject/TCardReader/Assets/TextAppear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAppear : MonoBehaviour {
+    int word = 0;
+    string[] options = new string[]{"test", "idk"};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string thisword = word[Random.Word(options)];
+        return thisword;
+    }
+}
+
