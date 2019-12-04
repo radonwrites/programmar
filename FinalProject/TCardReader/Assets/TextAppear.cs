d61466fa2387eb8d8ab04913fe4645b28b7b0fd1
Rachel <radonwrites@gmail.com>
2019-12-04 10:45:57 -0500

    new build b/c worried, then rememebered github...

--- FinalProject/TCardReader/Assets/TextAppear.cs
+++ FinalProject/TCardReader/Assets/TextAppear.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TextAppear : MonoBehaviour {
-    int word = 0;
-    string[] options = new string[]{"test", "idk"};
+    //static int word = 0;
+    //string[] options = new string[]{"test", "idk"};
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //int word = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        string thisword = word[Random.Word(options)];
-        return thisword;
+        //var aNoun : String[] = ["test", "idk"];
+        //var theNoun : String = aNoun[Random.Range(0, aNoun.Length)];
+        //string thisword = word[Random.Range(0, options.Length)];
+        //return thisword;
+        //with the word/thisword set couldn't figure out the following errors
+        //TextAppear.cs(20,27): error CS0021: Cannot apply indexing with [] to an expression of type 'int'
+        //Assets/TextAppear.cs(21,9): error CS0127: Since 'TextAppear.Update()' returns void, a return keyword must not be followed by an object expression
+
     }
 }
 
