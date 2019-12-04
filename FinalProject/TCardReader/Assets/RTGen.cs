b80fc30746995b4c177ec5518a4cab57d6d7ded3
Rachel <radonwrites@gmail.com>
2019-12-04 12:08:42 -0500

    testing various text mesh options

---
+++ FinalProject/TCardReader/Assets/RTGen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+[System.Runtime.InteropServices.ComVisible(true)]
+[System.Serializable]
+//public class Random
+
+public class RTGen : MonoBehaviour
+{
+    //public GameObject randomWord;
+    public Font font;
+    //public var Word;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = "testing";
+
+        /*
+        //getting this from Unity documentation on TextGenerator
+        //working from the ground up because I get errors I don't understand when building off the UI 
+        TextGenerationSettings settings = new TextGenerationSettings();
+        settings.textAnchor = TextAnchor.MiddleCenter;
+        settings.color = Color.red;
+        settings.generationExtents = new Vector2(500.0F, 200.0F);
+        settings.pivot = Vector2.zero;
+        settings.richText = true;
+        settings.font = font;
+        settings.fontSize = 32;
+        settings.fontStyle = FontStyle.Normal;
+        settings.verticalOverflow = VerticalWrapMode.Overflow;
+        TextGenerator generator = new TextGenerator();
+        generator.Populate("I am a string", settings);
+        Debug.Log("I generated: " + generator.vertexCount + " verts!");
+        */
+    }
+
+    // Update is called once per frame
+}
